ea05d9e897f50c45b4c3e55cdb6e52ea393ab53f
daga <daga@localhost>
2026-04-09 01:44:17 +0200

    merge

---
+++ proDavidMara/src/paqadiosmundo/AdiosMundo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqadiosmundo;
+
+/**
+ *
+ * @author daga
+ */
+public class AdiosMundo {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("adios mundo cruel");
+        
+        
+    }
+    
+}
